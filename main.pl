5a8851eba8301a0265dda1cae91e72e8e34ab274
mafor <lmaforl@mail.ru>
2012-12-21 16:09:02 +0600

    Task_2

--- main.pl
+++ main.pl
@@ -1,27 +1,15 @@
-@mas_file = grep($_ =~ m/[^\/?:*"><|]\.txt/i, @ARGV) or die("Enter the parameters"."\n");
-
+@mas_file = grep($_ =~ m/[^\/?:*"><|]\.txt$/i, @ARGV) or die("Enter the correct parameters"."\n");
 $exit_file = pop(@mas_file);
-
+open(FILE_OUT, ">$exit_file") or die("Error: $!");
+local $/ = "";
 foreach(@mas_file){
-	local $/ = "";
-	if(open(FILE, "<$_")){
-		eval('@t = <FILE>;');
-		if(length($@)>0){
-			print $@;
-		}
-		else{
-			eval('push(@number, @symbol2 = grep($_ !~ m/\D/i, @symbol = split(/\s+/, join(" ", @t))));');
-			print $@;
-		}
-	}
-	else{
-		print "Error: $!"."\n";
-	}
-	close(FILE);
+	eval{
+		open(FILE, "<$_");
+		@t = <FILE>;
+		push(@number, @symbol2 = grep($_ !~ m/\D/i, @symbol = split(/\s+/, join(" ", @t))));
+		close(FILE);
+	};
+	print $@;	
 }
-
-@number = sort {$a <=> $b} @number;
-
-open(FILE, ">$exit_file") or die("Error: $!");
-scalar(@number) == 0 ? print "Buffer is empty file could not be"."\n" : print FILE join(" ", @number);
-close(FILE);
+scalar(@number) == 0 ? print "Buffer is empty file could not be"."\n" : print FILE_OUT join(" ", @number = sort {$a <=> $b} @number);
+close(FILE_OUT);
